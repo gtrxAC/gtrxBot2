1fcfb87e57404430d22ef2dc2274fb7d89219482
gtrxAC <59177874+gtrxAC@users.noreply.github.com>
2019-12-25 15:38:59 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# gtrxBot2
-Utility discord bot with image editing commands
+# gtrxBot² (in early development)
+Utility discord bot (discord.js) with image editing commands.
+
+## Commands
+All commands must be prefixed with `gb`, for example `gbhelp`. You can change the prefix if self hosting.
+Commands can be viewed using the `help` command.
+
+### Utility commands
+`embed <title>|[description]|[footer]|[color]`: Creates an embed message.
+Example: `gbembed Embed Test|Lorem ipsum dolor sit amet|Note: I'm a footer|RED`
+`color` must match [this](https://discord.js.org/#/docs/main/stable/typedef/ColorResolvable) definition.
+
+`help [-p] [cmd]`: Shows available commands or info about one command.
+-p: Shows help in the current channel instead of DMs.
+cmd: Command to show info on.
+
+`invite`: Gets the invite link for the bot (if you're self hosting, change the link to your own bot's link)
+
+`nitro <emoji ...>`: Sends all the specified emoji, useful for sending nitro emoji without a subscription.
+Example: `gbemoji hmm aPepeReee aYouTried`
+
+`say <message ...>`: Sends a message as the bot account and deletes the command message.
+
+### Fun commands
+`kill [text/user]`: Kills the specified user in a random way. If no user is specified, it defaults to you.
+
+`emoji`: Gets a random emoji from discordemoji.com and shows it. This command is NSFW, because some emoji are NSFW.
+
+`sayas <user> <message ...>`: Says a command as a look-alike of another user, using webhooks.
+
+### Image commands
+All image commands operate on an attachment, image URL or the avatar of a mentioned user.
+`gbsomeimagecommand http://link.to/image.png`
+`gbsomeimagecommand @>gtrx<#6036`
+
+Image commands are currently our main focus, so expect more image commands soon.
+
+`iscale [image] <width> <height>`: Resizes an image.
+
+### Owner only commands
+`reload <command>`: Reloads a command file after it has been edited.
+
+## Self hosting
+Required modules:
+
+* discord.js (v11)
+* gm (for image editing commands)
+* node-fetch (for emoji command, uses json)
+* request (for image editing commands, uses stream/buffer)
+
+1. Install [node.js](https://nodejs.org/) and [npm](http://npmjs.com).
+2. Make a new folder, name it something you can remember.
+3. Open terminal and install modules. `npm install discord.js gm node-fetch request`
+4. Create a [bot account](https://discordapp.com/developers/applications), if you haven't already.
+5. Add your bot to a server, go to your app's page > OAuth2 > URL Generator > select "bot" and from the below list, choose "administrator". Copy the link to your browser and choose a server.
+6. Get the token for your bot, go to your app's page > Bot > Click to reveal token.
+7. Create a file called `config.json` and add the following content:
+
+```json
+{
+    "token": "Your token here",
+    "owner": "Your ID here",
+    "prefix": "Bot's prefix here, default is gb"
+}
+```
+[Need help getting your ID?](https://support.discordapp.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-)
+
+8. Run `bot.js`.
---
+++ bot.js
@@ -0,0 +1,117 @@
+//load required modules
+const fs = require("fs");
+const Discord = require('discord.js');
+
+//create a client, the bot itself
+const client = new Discord.Client();
+
+//load commands and make a list of cooldowns for them
+const cooldowns = new Discord.Collection();
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./cmds').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const command = require(`./cmds/${file}`);
+	client.commands.set(command.name, command);
+}
+
+//load configuration from config.json - this has the bot token, prefix and owner ID
+const config = require('./config.json');
+client.config = config;
+
+//log the ready message
+client.on('ready', () => {
+    console.log('________________________________________________________________________________');
+    console.log('');
+    console.log(`${client.user.username} is ready.`);
+    console.log(`* ID: ${client.user.id}`);
+    console.log(`* Guilds/Users: ${client.guilds.size}/${client.users.size}`);
+    console.log('________________________________________________________________________________');
+    console.log('');
+});
+
+//basic command handler. https://discordjs.guide/command-handling/
+client.on('message', (message) => {
+    //ignore non command messages and bot messages to prevent loops
+    if (!message.content.startsWith(config.prefix)) return;
+    if (message.author.bot) return;
+
+    //separate command and its arguments from the message
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const commandName = args.shift().toLowerCase();
+
+    //find the command from the loaded command files
+    const command = client.commands.get(commandName)
+        || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+    
+    //if not found, exit
+    if (!command) return;
+
+    //if command requires arguments and none are given, exit
+    if (command.args && !args.length) {
+        const embed = new Discord.RichEmbed()
+        .setColor(0x7289DA)
+        .setTitle('<:mdError:568466408250408970> Error')
+        .setDescription(`\`This command requires arguments!\n${command.name} ${command.usage}\``)
+        .setFooter(new Date().toISOString());
+        return message.channel.send(embed);
+    }
+
+    //if command is server only and used in DM, exit
+    if (command.guildOnly && message.channel.type !== 'text') {
+        const embed = new Discord.RichEmbed()
+        .setColor(0x7289DA)
+        .setTitle('<:mdError:568466408250408970> Error')
+        .setDescription(`\`This command is set to server only.\``)
+        .setFooter(new Date().toISOString());
+        return message.channel.send(embed);
+    }
+
+    //if command is owner only and used by non-owner, exit
+    if (command.ownerOnly && message.author.id !== config.owner) {
+        const embed = new Discord.RichEmbed()
+        .setColor(0x7289DA)
+        .setTitle('<:mdError:568466408250408970> Error')
+        .setDescription(`\`This command is set to owner only.\``)
+        .setFooter(new Date().toISOString());
+        return message.channel.send(embed);
+    }
+
+    //if command is on cooldown for that user, exit
+    if (!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Discord.Collection());
+    }
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown || 2) * 1000;
+    if (timestamps.has(message.author.id)) {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+        if (now < expirationTime) {
+            const timeLeft = (expirationTime - now) / 1000;
+            const embed = new Discord.RichEmbed()
+            .setColor(0x7289DA)
+            .setTitle('<:mdError:568466408250408970> Error')
+            .setDescription(`\`This command is in cooldown for ${timeLeft.toFixed(4)} seconds.\``)
+            .setFooter(new Date().toISOString());
+            return message.channel.send(embed);
+        }
+    }
+    
+    //execute command and add the cooldown timer
+    try {
+        command.execute(message, args);
+        timestamps.set(message.author.id, now);
+        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+    } catch (error) {
+        const embed = new Discord.RichEmbed()
+        .setColor(0x7289DA)
+        .setTitle('<:mdError:568466408250408970> Error')
+        .setDescription(`\`${error}\``)
+        .setFooter(new Date().toISOString());
+        message.channel.send(embed);
+        console.error(error);
+    }
+});
+
+//log in to Discord with the bot token in config.json
+//go to https://discordapp.com/developers/applications to get your own bot
+client.login(config.token);
